c3c75cf9e11d2c6a4075f73f5436b5f9579b1cd4
Codjo-Savio <codjosavio@gmail.com>
2026-03-13 16:28:16 +0100

    Created new test package file for each level of our applucation : Repository test, controller test, and service test

---
+++ backend/tinyfarm/src/test/java/com/api/tinyfarm/controller/UserControllerTest.java
@@ -0,0 +1,9 @@
+package com.api.tinyfarm.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserControllerTest {
+}
