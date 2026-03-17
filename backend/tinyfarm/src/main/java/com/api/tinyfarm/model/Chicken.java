f7133cc0dab4d5ec2ca97ee9686a8754d7c1a21c
Tugderic <108681381+Tugderic@users.noreply.github.com>
2026-03-17 10:26:17 +0000

    Chicken

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/model/Chicken.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/model/Chicken.java
@@ -1,4 +1,30 @@
 package com.api.tinyfarm.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chicken")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Chicken {
+
+    @Id
+    @Column(name = "a_id")
+    private Integer aId;
+
+    enum chickenTypeEnum{poussin,poule,coq}
+
+    @Column(name = "chicken_type")
+    private chickenTypeEnum chickenType;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "fasting")
+    private Integer fasting;
 }
