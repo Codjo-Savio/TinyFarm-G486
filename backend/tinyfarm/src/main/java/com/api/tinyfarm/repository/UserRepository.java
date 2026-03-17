0fbe2ba1635387424c93b23e273a61c80fd2edaf
Razzmo <boucher17out@gmail.com>
2026-03-17 08:57:17 +0100

    Add Controller, Service for animals.

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/UserRepository.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/UserRepository.java
@@ -1,16 +1,16 @@
 package com.api.tinyfarm.repository;
 
 import com.api.tinyfarm.model.User;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
     Optional<User> findByName(String name);
     Optional<User> findById(Long id);
     Optional<List<User>> findByLevel(Integer level);
+    Optional<List<User>> findByGender(User.Gender gender);
+    Optional<List<User>> findByEcus(Integer ecus);
 }
