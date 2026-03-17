441e6c21f2b4dc973f1ba37457267c885a04e8a3
Noa CHUPIN <noa.chupin@etu.univ-nantes.fr>
2026-03-17 09:09:44 +0100

    Solving issues

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/AnimalRepository.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/AnimalRepository.java
@@ -6,10 +6,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnimalRepository {
     // Animal
 
-    Optional<List<Animal>> findByAll();
     Optional<Animal> findByName(String name);
     Optional<Animal> findByUID(int UID);
     Optional<List<Animal>> findByClean(Boolean clean);
