f54060557ac2eda238d6c38dedbac151e6721ff9
Tugderic <108681381+Tugderic@users.noreply.github.com>
2026-03-17 08:07:36 +0000

    ChickenService ~~~

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/service/ChickenService.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/service/ChickenService.java
@@ -1,7 +1,46 @@
 package com.api.tinyfarm.service;
 
+import com.api.tinyfarm.model.Chicken;
+import com.api.tinyfarm.repository.Chickenepository;
+
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ChickenService {
+
+    private final ChickenRepository chickenRepository;
+
+    public ChickenService(ChickenRepository chickenRepository) {
+        this.chickenRepository = chickenRepository;
+    }
+
+    public List<Chicken> findAll() {
+        return chickenRepository.findAll();
+    }
+
+
+    public Chicken findById(Long id) {
+        return ChickenRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Poulet introuvale : " + id));
+    }
+
+    public Chicken create(Chicken chicken) {
+        return chickenRepository.save(chicken);
+    }
+
+    public Chicken update(Long id, Chicken modificatedChicken) {
+        Chicken existing = findById(id);
+        existing.setAid(modificatedChicken.getAid());
+        existing.setChickenType(modificatedChicken.getChickenType());
+        existing.setName(modificatedChicken.getName());
+        existing.setFasting(modificatedChicken.getFasting());
+        return chickenRepository.save(existing);
+    }
+
+    public void delete(Long id) {
+        chickenRepository.deleteById(id);
+    }
 }
+
