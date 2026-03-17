4bd8e8946e771f8644cac662ee4e6ed08be2f419
Nono_56L <168830565+Nono56L@users.noreply.github.com>
2026-03-17 08:12:27 +0000

    modification du service et du repository

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/ProductRepository.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.api.tinyfarm.repository;
 
-import com.api.tinyfarm.model.User;
+import com.api.tinyfarm.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- backend/tinyfarm/src/main/java/com/api/tinyfarm/service/ProductService.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/service/ProductService.java
@@ -1,7 +1,48 @@
 package com.api.tinyfarm.service;
 
+import com.api.tinyfarm.model.Product;
+import com.api.tinyfarm.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public void deleteAllUsers(){
+       productRepository.deleteAll();
+    }
+
+    public User findById(Long id) {
+        return productRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Produit introuvale : " + id));
+    }
+
+    public User create(Product product) {
+        return userRepository.save(product);
+    }
+    /* TODO
+    public User update(Long id, User modificatedProduct) {
+        Product existing = findById(id);
+        existing.setName(modificatedUser.getName());
+        existing.setGender(modificatedUser.getGender());
+        existing.setEcus(modificatedUser.getEcus());
+        existing.setLevel(modificatedUser.getLevel());
+        return productRepository.save(existing);
+    } */
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
 }
