8ea7913b1ad186b6eda27dd8a5ed2554a40f85a2
Evan <evan.garrec@etu.univ-nantes.fr>
2026-03-17 08:15:48 +0000

    AnimalRepository ~

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/AnimalRepository.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/AnimalRepository.java
@@ -1,4 +1,14 @@
 package com.api.tinyfarm.repository;
 
-public interface AnimalRepository {
+import com.api.tinyfarm.model.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    
+    Optional<Animal> findById(Long id);
 }
