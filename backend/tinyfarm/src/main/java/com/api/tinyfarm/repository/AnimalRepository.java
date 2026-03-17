394beb6015479b6eae75183d22156668f966de39
Noa CHUPIN <noa.chupin@etu.univ-nantes.fr>
2026-03-17 09:07:07 +0100

    Adding AnimalRepository

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/AnimalRepository.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/AnimalRepository.java
@@ -1,4 +1,20 @@
 package com.api.tinyfarm.repository;
 
+import com.api.tinyfarm.model.Animal;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 public interface AnimalRepository {
+    // Animal
+
+    Optional<List<Animal>> findByAll();
+    Optional<Animal> findByName(String name);
+    Optional<Animal> findByUID(int UID);
+    Optional<List<Animal>> findByClean(Boolean clean);
+    Optional<List<Animal>> findByHealthy(Boolean healthy);
+    Optional<List<Animal>> findByAge(int age);
+    Optional<List<Animal>> findByWeight(float weight);
+    Optional<List<Animal>> findByGender(Boolean gender);
 }
