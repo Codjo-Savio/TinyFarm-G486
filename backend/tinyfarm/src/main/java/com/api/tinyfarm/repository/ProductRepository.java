59765cce2d35b86b1a8c151936fd872c7219fd12
Nono_56L <168830565+Nono56L@users.noreply.github.com>
2026-03-17 08:00:10 +0000

    mise à jour du repository

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/ProductRepository.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/repository/ProductRepository.java
@@ -1,4 +1,14 @@
 package com.api.tinyfarm.repository;
 
-public interface ProductRepository {
+import com.api.tinyfarm.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<List<Product>> findAll();
+    Optional<Product> findById(Long id);
 }
