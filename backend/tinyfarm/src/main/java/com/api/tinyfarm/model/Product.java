33eb9ce8136f6f5f29a019b1c74ceb0e6d920b46
Nono_56L <168830565+Nono56L@users.noreply.github.com>
2026-03-17 07:54:25 +0000

    mise a jour du product model

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/model/Product.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/model/Product.java
@@ -1,4 +1,39 @@
 package com.api.tinyfarm.model;
 
+import javax.annotation.processing.Generated;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "`product`")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+
+
+    @Id
+    @Column(name = "productID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "description")
+    String description;
+
+    @Column(name = "collection")
+    Boolean collection;
+
+    @Column(name = "price")
+    Float price;
+
+    @Column(name = "coef")
+    Integer coef;
+
+    @PrePersist
+    public void prePersist() {
+        this.coef = 1;
+    }
 }
