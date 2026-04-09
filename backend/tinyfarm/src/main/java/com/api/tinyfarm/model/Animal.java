4ad010d75b80143fc36062a87105d0737f2092f0
Codjo-Savio <codjosavio@gmail.com>
2026-04-09 14:44:18 +0200

    End of day endpoint added

--- backend/tinyfarm/src/main/java/com/api/tinyfarm/model/Animal.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/model/Animal.java
@@ -79,4 +79,6 @@ public class Animal {
             wateredToday = true;
         }
     }
+
+
 }
--- backend/tinyfarm/src/main/java/com/api/tinyfarm/service/ChickenService.java
+++ backend/tinyfarm/src/main/java/com/api/tinyfarm/service/ChickenService.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ChickenService {
+public class ChickenService{
 
     private final ChickenRepository chickenRepository;
     private final UserService userService;
