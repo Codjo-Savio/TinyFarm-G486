cf15fa62d16cceaa402e853c31f99ab08111ad2d
Mlb <97614168+devmlb@users.noreply.github.com>
2026-04-30 13:27:12 +0200

    Fixed 401 error when loading bottom actions marketplace overview

--- frontend/src/dashboard/trade/marketplace/script.js
+++ frontend/src/dashboard/trade/marketplace/script.js
@@ -24,7 +24,7 @@ async function getCurrentUserId() {
 }
 
 async function fetchAllMarkets() {
-    const response = await fetchApiWithCredentials(`/market/not/me`);
+    const response = await fetchApiWithCredentials("/market/not/me");
     if (!response.ok) {
         throw new Error(`Erreur HTTP : ${response.status}`);
     }
